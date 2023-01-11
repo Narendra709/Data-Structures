0374fd8e62bcc0133fc3bc98ba167540c7307d4b
Narendra709 <113838269+Narendra709@users.noreply.github.com>
2023-01-11 12:30:20 +0530

    Add files via upload

---
+++ array insertion and deletion.cpp
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main()
+{
+	int arr[25],size,choise=0,ins,del,dis;
+	printf("enter the no.of elements");
+	scanf("%d",&size);
+	printf("enter the elements");
+	printf("\n\t 1.ins\n\t 2.del\n\t 3.dis\n\t");
+	printf("enter the choise");
+	scanf("%d",&choise);
+	printf("taken",choise);
+	 
+}
